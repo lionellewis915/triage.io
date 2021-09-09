14e75a1ac2a5533d3147a5baea0a86cc5273f3f5
Lionel Lewis <lionellewis915@gmail.com>
2021-09-09 11:44:12 -0500

    Connected activation front-end to back-end

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,31 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import App from './App';
 import Register from './Screens/Register';
+import Activate from './Screens/Activation';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Switch>
-        <Route path='/' exact render={props => <App {...props}/>}/>
+  <BrowserRouter>
+    <Switch>
+      <Route
+        path="/" 
+        exact 
+        render={(props) => <App {...props} />}
+      />
 
-        <Route path='/register' exact render={props => <Register {...props}/>}/>
-      </Switch>
-    </BrowserRouter>,
-  document.getElementById('root')
-)
+      <Route
+        path="/register"
+        exact
+        render={(props) => <Register {...props} />}
+      />
+
+      <Route
+        path="/users/activate/:token"
+        exact
+        render={(props) => <Activate {...props} />}
+      />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
