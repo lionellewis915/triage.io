6080553dbc3f6c090e58ec0f70525549c341b69b
Lionel Lewis <lionellewis915@gmail.com>
2021-09-09 09:13:40 -0500

    Implemented the validation script in authentication route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// Validation
+const {
+    validLogin,
+    validRegister,
+    forgotPasswordValidator,
+    resetPasswordValidator
+} = require('../client/src/helpers/valid')
+
 // Load controllers
 const {
-    registerController
+    registerController,
+    activationController
 } = require('../controllers/auth.controller.js')
-router.post('/register', registerController)
+router.post('/register', validRegister, registerController)
+router.post('/actiavtion', activationController)
 
 module.exports = router
